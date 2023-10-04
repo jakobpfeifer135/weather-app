04c4a7295a8eacf46c0e980de3cfc22d97ca5f88
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-03 21:09:35 -0500

    trying to add a working card system into the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,12 +38,13 @@ function fetchCityData(city) {
             var lon = data.coord.lon;
 
             nameOfCity.textContent = data.name;
+           
             cityContainer.append(nameOfCity)
-            wind.textContent = data.wind.speed;
+            wind.textContent = "Wind speed: " + data.wind.speed + " MPH";
             cityContainer.append(wind)
-            humidity.textContent = data.main.humidity;
+            humidity.textContent = "Humidity: " + data.main.humidity + "%";
             cityContainer.append(humidity)
-            temperature.textContent = data.main.temp;
+            temperature.textContent = "temperature: " + data.main.temp + " F";
             cityContainer.append(temperature)
             var cityIcon = document.createElement("img")
             cityIcon.setAttribute("src", icon)
@@ -61,21 +62,24 @@ function fetchCityData(city) {
             })
             .then (function(forecastData){
                 console.log(forecastData);
-                for (var i = 0; i < 6; i++){
+                for (var i = 0; i < 5; i++){
+                    
                     var fiveDayForecastDate = document.createElement("h3")
                     fiveDayForecastDate.textContent = dayjs().add(i,"days").format("DD/MM/YYYY")
                     forecast.append(fiveDayForecastDate);
-                    var fiveDayTemp = document.createElement("h3")
-                    fiveDayTemp = ;
+                    var fiveDayTemp = document.createElement("h4")
+                    fiveDayTemp = forecastData.daily[i].temp.day;
+                    forecast.append(fiveDayTemp);
+                    var fiveDayWind = document.createElement("h4")
+                    fiveDayWind = forecastData.daily[i].wind_speed;
+                    forecast.append(fiveDayWind);
+                    var fiveDayHumidity = document.createElement("h4");
+                    fiveDayHumidity = forecastData.daily[i].humidity;
+                    forecast.append(fiveDayHumidity);
+                   
 
                 }
             })
-
-
-
-
-
-
 
             console.log("Weather data for " + city + ":");
             console.log(data);
@@ -86,41 +90,3 @@ function fetchCityData(city) {
 }
 
 
-//TODO MAJOR IMPLEMENTATIONS....
-
-//TODO implement a search bar
-
-//TODO add a form input otherwise known as search-bar that saves its users input in local storage and appends the name to the page under history
-
-//TODO display current and future weather conditions
-
-//TODO when we search for a city it displays: the cities name, the date, a corresponding icon for the weather, the temperature, wind speeds and humidity for the respective city
-
-//TODO when our city has been selected display a 5 day forecast
-
-//TODO when i click on each city saved in my history section it will display that cities information instead
-
-//~ MINOR IMPLEMENTATIONS
-
-//~ get bootstrap up and operational
-
-//~ get css updated to update user interface
-
-//~ get html's code from div's to semantic structure because of bootstrap
-
-//~ get javascript var's that correspond to given class's and id's in html
-
-//~ implement the weather api into our js and log its values
-
-//~ link the search bar to local storage, and set/get/append to page with weather api
-
-//~ add a dayjs into code to allow for time and date to be applied to the page use an interval to update in real time
-
-//~  connect my current city box to match the selected city
-
-
-
-// Create an HTML form with an input field and a submit button.
-// Use JavaScript to handle form submission and retrieve the user's input.
-// Store the user's input in local storage.
-// Append the user's input (city name) to the history section on the page.
