d354661b5c256fa4135e3014d352b6a2eeadc53c
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-10-03 21:32:11 -0500

    adjusted the javascript to allow for individual line placement of each weather condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,24 @@
 
+
 var APIKey = "f30dc0b71f772a037a522282770190be";
 var searchBar = document.querySelector("#search-form");
-var cityInput = document.querySelector("#city-input")
-var cityContainer = document.querySelector("#cityContainer")
-var forecast = document.querySelector("#forecast")
-var today = dayjs().format("MM/DD/YYYY")
+var cityInput = document.querySelector("#city-input");
+var cityContainer = document.querySelector("#cityContainer");
+var forecast = document.querySelector("#forecast");
+var today = dayjs().format("MM/DD/YYYY");
 
 searchBar.addEventListener("submit", function (event) {
     event.preventDefault();
-    // var inputField = searchBar.querySelector("input[type='text']");
-    // var city;
-    // Update the queryURL with the user's input
 
-    var cityValue = cityInput.value
-    fetchCityData(cityValue)
+    var cityValue = cityInput.value;
+    fetchCityData(cityValue);
 });
-
 
 function fetchCityData(city) {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
     cityContainer.innerHTML = "";
     forecast.innerHTML = "";
-    // Fetch the weather data for the user's input city
+
     fetch(queryURL)
         .then(function (response) {
             return response.json();
@@ -39,54 +36,55 @@ function fetchCityData(city) {
 
             nameOfCity.textContent = data.name;
            
-            cityContainer.append(nameOfCity)
+            cityContainer.append(nameOfCity);
             wind.textContent = "Wind speed: " + data.wind.speed + " MPH";
-            cityContainer.append(wind)
+            cityContainer.append(wind);
             humidity.textContent = "Humidity: " + data.main.humidity + "%";
-            cityContainer.append(humidity)
-            temperature.textContent = "temperature: " + data.main.temp + " F";
-            cityContainer.append(temperature)
-            var cityIcon = document.createElement("img")
-            cityIcon.setAttribute("src", icon)
-            cityContainer.append(cityIcon)
-            //cityIcon.classlist.add("")
+            cityContainer.append(humidity);
+            temperature.textContent = "Temperature: " + data.main.temp + " F";
+            cityContainer.append(temperature);
+            var cityIcon = document.createElement("img");
+            cityIcon.setAttribute("src", icon);
+            cityContainer.append(cityIcon);
 
-            // Handle the weather data here, for example:
-            var fiveDayUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIKey + "&units=imperial";
-            // var fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
-            
+            var fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIKey + "&units=imperial";
 
             fetch(fiveDayUrl)
-            .then(function(response){
-                return response.json()
-            })
-            .then (function(forecastData){
-                console.log(forecastData);
-                for (var i = 0; i < 5; i++){
-                    
-                    var fiveDayForecastDate = document.createElement("h3")
-                    fiveDayForecastDate.textContent = dayjs().add(i,"days").format("DD/MM/YYYY")
-                    forecast.append(fiveDayForecastDate);
-                    var fiveDayTemp = document.createElement("h4")
-                    fiveDayTemp = forecastData.daily[i].temp.day;
-                    forecast.append(fiveDayTemp);
-                    var fiveDayWind = document.createElement("h4")
-                    fiveDayWind = forecastData.daily[i].wind_speed;
-                    forecast.append(fiveDayWind);
-                    var fiveDayHumidity = document.createElement("h4");
-                    fiveDayHumidity = forecastData.daily[i].humidity;
-                    forecast.append(fiveDayHumidity);
-                   
+                .then(function(response){
+                    return response.json();
+                })
+                .then (function(forecastData){
+                    console.log(forecastData);
+                    for (var i = 0; i < 5; i++){
+                        var forecastCard = document.createElement("div");
+                        forecastCard.className = "card col-lg-2 m-2";
 
-                }
-            })
+                        var cardDate = document.createElement("h3");
+                        cardDate.textContent = dayjs().add(i,"days").format("DD/MM/YYYY");
+                        forecastCard.append(cardDate);
 
-            console.log("Weather data for " + city + ":");
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.log("An error occurred while fetching weather data: " + error);
+                        var cardIcon = document.createElement("img");
+                        var iconCode = forecastData.daily[i].weather[0].icon;
+                        cardIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + iconCode + ".png");
+                        forecastCard.append(cardIcon);
+
+                        var cardTemp = document.createElement("p");
+                        cardTemp.textContent = "Temperature: " + forecastData.daily[i].temp.day + " F";
+                        forecastCard.append(cardTemp);
+
+                        var cardWind = document.createElement("p");
+                        cardWind.textContent = "Wind Speed: " + forecastData.daily[i].wind_speed + " MPH";
+                        forecastCard.append(cardWind);
+
+                        var cardHumidity = document.createElement("p");
+                        cardHumidity.textContent = "Humidity: " + forecastData.daily[i].humidity + "%";
+                        forecastCard.append(cardHumidity);
+
+                        forecast.append(forecastCard);
+                    }
+                })
+                .catch(function (error) {
+                    console.log("An error occurred while fetching weather data: " + error);
+                });
         });
 }
-
-
